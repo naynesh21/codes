6edbb703f3d18bdb47cba1c6f339b93f2b5c0fe0
naynesh21 <nayneshpatil62@gmail.com>
2024-12-15 10:16:05 +0530

    Update Exp2.py

--- Exp2.py
+++ Exp2.py
@@ -1,4 +1,10 @@
 # Function for average score of the class
+#Write a Python program to store marks scored in subject “Fundamental of DataStructure”
+#by N students in the class. Write functions to compute following:
+#a) The average score of class
+#b) Highest score and lowest score of class
+#c) Count of students who were absent for the test
+#d) Display mark with highest frequency
 
 def average(listofmarks):
     sum=0
